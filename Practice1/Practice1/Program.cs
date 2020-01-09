31a0cb4f20cce977162398690c0aba3ca9286725
lsaltz <theelleris13x@gmail.com>
2020-01-08 22:55:15 -0700

    view datatable

--- Practice1/Practice1/Program.cs
+++ Practice1/Practice1/Program.cs
@@ -9,13 +9,15 @@ namespace Practice1
             Users userClass = new Users();
             ReportCard rc = new ReportCard();
 
+            rc.Data();
+
             string newUser = "n";
             string firstInput;
             string user = "e";
+            bool x = false;
 
             do
             {
-
                 Console.WriteLine("Press 'e' to enter in Login Credentials. If you are a new user, press 'n' to sign up for an account.");
                 firstInput = Console.ReadLine();
                 if (firstInput == newUser)
@@ -28,8 +30,15 @@ namespace Practice1
                 }
                 else
                 {
+                    x = true;
                     Console.WriteLine("Input is invalid. Try again.");
                 }
-            } while (firstInput != newUser || firstInput != user);
+
+            } while (x);
+
+            Console.WriteLine("Press any key to view Report Cards.");
+            Console.ReadLine();
+            rc.DisplayData();
         }
+    }
 }
--- Practice1/Practice1/ReportCard.cs
+++ Practice1/Practice1/ReportCard.cs
@@ -6,28 +6,54 @@ namespace Practice1
 {
     public class ReportCard
     {
+
+        System.Data.DataTable report = new System.Data.DataTable();
+
+        public void Data()
+        {
+            try
+            {
+                report.Clear();
+                report.Columns.Add("Name");
+                report.Columns.Add("Grade");
+
+                System.Data.DataRow marshall = report.NewRow();
+                marshall["Name"] = "Marshall";
+                marshall["Grade"] = "0";
+                report.Rows.Add(marshall);
+
+                System.Data.DataRow todrick = report.NewRow();
+                todrick["Name"] = "Todrick";
+                todrick["Grade"] = "0";
+                report.Rows.Add(todrick);
+
+                System.Data.DataRow aria = report.NewRow();
+                aria["Name"] = "Aria";
+                aria["Grade"] = "0";
+                report.Rows.Add(aria);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            } 
+        }
         public void DisplayData() {
-            System.Data.DataTable report = new System.Data.DataTable();
-            report.Clear();
-            report.Columns.Add("Name");
-            report.Columns.Add("Grade");
-           
-            System.Data.DataRow marshall = report.NewRow();
-            marshall["Name"] = "Marshall";
-            marshall["Grade"] = "0";
-            report.Rows.Add(marshall);
 
-            System.Data.DataRow todrick = report.NewRow();
-            todrick["Name"] = "Todrick";
-            todrick["Grade"] = "0";
-            report.Rows.Add(todrick);
+            foreach (System.Data.DataColumn column in report.Columns)
+            {
+                Console.Write("\t{0}", column.ColumnName);
+            }
+            Console.WriteLine("\t");
 
-            System.Data.DataRow aria = report.NewRow();
-            aria["Name"] = "Aria";
-            aria["Grade"] = "0";
-            report.Rows.Add(aria);
+            foreach (System.Data.DataRow row in report.Rows)
+            {
+                foreach (System.Data.DataColumn column in report.Columns)
+                {
+                    Console.Write("\t{0}", row[column]);
+                }
 
-            Console.WriteLine(report);
+                Console.WriteLine("\t");
+            }
         }
     }
 }
